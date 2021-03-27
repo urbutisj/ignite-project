dd8154b910d34561e3b5594ba09cf50218fa1e75
Justinas <urbutisj@gmail.com>
2021-03-27 13:11:26 +0200

    7. Fetch Game Details

--- src/api.js
+++ src/api.js
@@ -42,3 +42,11 @@ const new_games = `games?key=${process.env.REACT_APP_RAWG_API}&dates=${lastYear}
 export const popularGamesURL = () => `${base_url}${popular_games}`;
 export const upcomingGamesURL = () => `${base_url}${upcoming_games}`;
 export const newGamesURL = () => `${base_url}${new_games}`;
+
+// Game Details
+export const gameDetailsURL = (game_id) =>
+  `${base_url}games/${game_id}?key=${process.env.REACT_APP_RAWG_API}`;
+
+//Game Screenshots
+export const gameScreenshotURL = (game_id) =>
+  `${base_url}games/${game_id}/screenshots?key=${process.env.REACT_APP_RAWG_API}`;
